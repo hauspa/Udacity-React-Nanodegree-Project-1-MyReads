3d05c1cacc53e4fa93d930c477e44d05a97a6dad
Pasci <hausberger.pascal@gmail.com>
2018-11-17 17:49:18 +0900

    feat: BooksUI, skeleton
    implementing BooksUI.

---
+++ src/BooksUI.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import PropTypes from 'react-proptypes'
+
+
+class BooksUI extends Component {
+  render(){
+    return(
+      
+    );
+  }
+}
+
+BooksUI.propTypes = {
+  books: PropTypes.array.isRequired
+}
