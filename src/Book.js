1dccd636f3093811987bf0604dff71a120f7f7ba
Pasci <hausberger.pascal@gmail.com>
2018-11-26 17:13:51 +0900

    refactor: Status Component
    moved status options to Status Component and displaying the component inside Book Component

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'react-proptypes'
+import Status from './Status'
 
 class Book extends Component {
 
@@ -24,15 +25,7 @@ class Book extends Component {
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-            <div className="book-shelf-changer">
-              <select>
-                <option value="move" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
+            <Status />
           </div>
           <div className="book-title">{book.title}</div>
           <div className="book-authors">{this.displayAuthors(book.authors)}</div>
---
+++ src/Status.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import PropTypes from 'react-proptypes'
+
+class Status extends Component {
+  render(){
+    return (
+      <div className="book-shelf-changer">
+        <select>
+          <option value="move" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+Status.propTypes = {
+
+}
+
+export default Status
