0f8ac09b5244e69c9edfb1f2a878603e8eefc9e4
Pasci <hausberger.pascal@gmail.com>
2018-11-26 17:36:53 +0900

    feat: show current shelf in Status
    displays current shelf

--- src/Book.js
+++ src/Book.js
@@ -25,7 +25,7 @@ class Book extends Component {
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-            <Status />
+            <Status shelfInfo={book.shelf} />
           </div>
           <div className="book-title">{book.title}</div>
           <div className="book-authors">{this.displayAuthors(book.authors)}</div>
--- src/Status.js
+++ src/Status.js
@@ -2,10 +2,26 @@ import React, {Component} from 'react'
 import PropTypes from 'react-proptypes'
 
 class Status extends Component {
+
+  state = {
+
+  }
+
+  componentDidMount = () => {
+    console.log("Status: " + this.props.shelfInfo);
+  }
+
+  handleChange = () => {
+    // change state to reflect what user setState
+
+    // send to parent component to update the BooksAPI DB 
+  }
+
+
   render(){
     return (
       <div className="book-shelf-changer">
-        <select>
+        <select value={this.props.shelfInfo} onChange={this.handleChange}>
           <option value="move" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
@@ -18,7 +34,7 @@ class Status extends Component {
 }
 
 Status.propTypes = {
-
+  shelfInfo: PropTypes.string.isRequired
 }
 
 export default Status
