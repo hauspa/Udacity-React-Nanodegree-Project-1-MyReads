d4fddd515edbb00bc6b8a727b90b341333869d55
Pasci <hausberger.pascal@gmail.com>
2018-11-26 18:07:32 +0900

    feat: set and show new shelf
    can change shelf and it shows current shelf in Status. Now gotta save in parent component, so that it is updated in BooksAPI & also so that the book changes the shelf!

--- src/Status.js
+++ src/Status.js
@@ -4,24 +4,43 @@ import PropTypes from 'react-proptypes'
 class Status extends Component {
 
   state = {
-
+    shelf: ""
   }
 
   componentDidMount = () => {
     console.log("Status: " + this.props.shelfInfo);
+
   }
 
-  handleChange = () => {
-    // change state to reflect what user setState
+  showSelected = () => {
+    // since better not set state to props, gonna leave state empty at first and only use state once user changes original shelf
 
-    // send to parent component to update the BooksAPI DB 
+    // user has changed the shelf => use state
+    if(this.state.shelf.length > 0){
+      return this.state.shelf;
+    }
+    //user has not changed shelf => use props
+    else{
+      return this.props.shelfInfo;
+    }
+  }
+
+  handleChange = (event) => {
+    let targetValue = event.target.value; // gotta save as property first, otherwise bug/crash because of asynch
+
+    // change state to reflect what user setState
+    this.setState((prevState) => ({
+      shelf: targetValue
+    }));
+    // send to parent component to update the BooksAPI DB
+    
   }
 
 
   render(){
     return (
       <div className="book-shelf-changer">
-        <select value={this.props.shelfInfo} onChange={this.handleChange}>
+        <select value={this.showSelected()} onChange={this.handleChange}>
           <option value="move" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
